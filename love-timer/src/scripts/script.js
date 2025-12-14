b69cabdb67c7830d18d3f945e4a849c7ee53c110
Mateusz Tobiasz <mati11.2001@gmail.com>
2025-12-15 00:22:18 +0100

    Updated js script to show only positive values and hide lower text when we are together

--- love-timer/src/scripts/script.js
+++ love-timer/src/scripts/script.js
@@ -1,21 +1,28 @@
-const meetingDate = new Date("2025-12-16T18:00:00");
+const meetingDate = new Date("2025-12-16T22:00:00");
 
 function updateTimer() {
-  const now = new Date();
-  const diff = meetingDate - now;
+    const timer = document.getElementById("timer")
+    const bottomText = document.querySelector(".bottom-text")
+    const now = new Date();
+    const diff = meetingDate - now;
 
-  if (diff <= 0) {
-    document.getElementsByClassName("timer")[0].textContent = "We're together ❤️";
-    return;
-  }
+    if (diff <= 0) {
+        timer.textContent = "We're together 🐻";
+        bottomText.hidden = true
 
-  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-  const minutes = Math.floor((diff / (1000 * 60)) % 60);
-  const seconds = Math.floor((diff / 1000) % 60);
+        return;
+    }
 
-  document.getElementsByClassName("timer")[0].textContent =
-    `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((diff / (1000 * 60)) % 60);
+    const seconds = Math.floor((diff / 1000) % 60);
+
+    timer.textContent = ''
+    if (days != 0) {timer.textContent += `${days}d `}
+    if (hours != 0) {timer.textContent += `${hours}h `}
+    if (minutes != 0) {timer.textContent += `${minutes}m `}
+    if (seconds != 0) {timer.textContent += `${seconds}s`}
 }
 
 setInterval(updateTimer, 1000);
